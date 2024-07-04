b3a49be9a11b082871354f119303652e9396d5e5
Jesus MT <ijesus.mt@gmail.com>
2024-07-04 16:39:14 -0400

    Mostrar nombre de usuario en pantalla

--- force-app/main/default/lwc/familiaBurron/familiaBurron.js
+++ force-app/main/default/lwc/familiaBurron/familiaBurron.js
@@ -1,9 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import COLUMNA_NOMBRE from '@salesforce/schema/User.Name';
+const columnas = [COLUMNA_NOMBRE];
 
 export default class FamilaBurron extends LightningElement {
     personajeSeleccionadoId;
 
     onPersonajeSeleccionado(event){
-        this.personajeSeleccionadoId = event.detail;
+        this.personajeSelecciona
+        doId = event.detail;
     }
+
+    userId = Id;
+    @wire(getRecord, {recordId: '$userId', fields: columnas})
+    user;
+    get name(){
+        console.log('this.user.data', JSON.stringify(this.user.data));
+        console.log('Id:',Id);
+        return getFieldValue(this.user.data, COLUMNA_NOMBRE);
+    }    
 }
